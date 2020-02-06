8629c81cc0163f5f43ebdefe65bc693d70e268dc
vutienluc1991 <48780352+vutienluc1991@users.noreply.github.com>
2020-02-06 23:42:52 +0700

    Add files via upload

---
+++ data.h
@@ -0,0 +1,20 @@
+#ifndef DATA_H
+#endif DATA_H
+
+#include "EEPROM.h"
+
+//Configuration
+//wifi ssid
+#define LOC_WIFI_SSID 0
+//wifi password
+#define LOC_WIFI_PASS 64
+//config threshold
+#define LOC_DATA 128
+
+class data{
+  public:
+    
+  private:
+  
+  
+}
