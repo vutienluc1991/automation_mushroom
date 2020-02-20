6239822b74376cbdd83baa02ad21e40e996c0047
vutienluc1991 <48780352+vutienluc1991@users.noreply.github.com>
2020-02-20 18:25:53 +0700

    Update data.cpp

--- data.cpp
+++ data.cpp
@@ -1,1 +1,40 @@
 
+#include "data.h"
+
+bool DATA::ee_init(){
+  if (!EEPROM.begin(EEPROM_SIZE))
+  {
+    this->status = 0;
+    return 0;
+  }
+  else{
+    this->status = 1;
+    return 1;
+  }
+}
+
+bool DATA::ee_write(int addr, int size, char* data){
+  if(this->status) return 0; // means failed to write
+  int len = sizeof(data);
+  if(len >= size)
+    len = size; // Avoid buffer-overflow
+  // write data
+  for(int i = 0; i < len; i++){
+    EEPROM.write(addr + i, data[i]);
+  }
+  // clear old data afterward
+  for(int i = len; i < size; i++){
+    EEPROM.write(addr + i, 0);
+  }
+  
+  EEPROM.commit(); // Commit after done writing
+  return 1; // means writing successfully
+}
+
+bool DATA::ee_read(int addr, int size, char* buf){
+  if(this->status) return 0; // means failed to read
+  for(int i = 0; i < size; i++){
+    buf[i] = EEPROM.read(addr + i);
+  }
+  return 1; // means reading successfully 
+}
