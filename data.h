8b69936226447e9f14bb7f499b23439ed530b326
vutienluc1991 <48780352+vutienluc1991@users.noreply.github.com>
2020-02-20 18:26:19 +0700

    Update data.h

--- data.h
+++ data.h
@@ -3,18 +3,31 @@
 
 #include "EEPROM.h"
 
-//Configuration
-//wifi ssid
-#define LOC_WIFI_SSID 0
-//wifi password
-#define LOC_WIFI_PASS 64
-//config threshold
-#define LOC_DATA 128
+#define EEPROM_SIZE 512
 
-class data{
+//Configuration
+#define EE_CONFIG 0
+//The first 32 bytes is used only for configuration 
+// - such as wifi mode or bluetooth mode
+// - Which pin to use... etc...
+
+
+//wifi ssid - the next 64 bytes
+#define EE_WIFI_SSID 32
+//wifi password - the next 32 bytes
+#define EE_WIFI_PASS 96
+
+//what left is for data - Remember there is only 512 Bytes of EEPROOM so use it wisely
+// Should contains - schedule by time of the day, by threshold, default pump time
+#define EE_DATA 128
+
+// There are 512B of eeprom so use it wisely
+class DATA{
   public:
-    
+    bool ee_write(int addr, int size, char* data);
+    bool ee_read(int addr, int size, char* buf);
+    bool ee_init();
   private:
-  
-  
-}
+    // Something need to keep private then lol
+    bool status = 0;
+};
