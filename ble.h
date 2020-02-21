0cd5a25634d817de4601df7d0b6002a0a0bdbc71
vutienluc1991 <48780352+vutienluc1991@users.noreply.github.com>
2020-02-21 18:36:09 +0700

    Update ble.h

--- ble.h
+++ ble.h
@@ -1,1 +1,31 @@
 
+#include "BluetoothSerial.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
+
+class BLE{
+  public:
+    bool ble_init(char* name);
+    
+    // reading 
+    bool ble_read_data(char* buf, int size);
+    bool ble_write_data(char* data);
+    
+  private:
+    BluetoothSerial ble;
+};
+
+//void callback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param){
+//  if(event == ESP_SPP_SRV_OPEN_EVT){
+//    Serial.println("Client Connected");
+//  }
+// 
+//  if(event == ESP_SPP_CLOSE_EVT ){
+//    Serial.println("Client disconnected");
+//  }
+//}
+
