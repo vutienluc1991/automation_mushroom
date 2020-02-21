32ced33cdbf9e64b06919cd11b840de9f779de42
vutienluc1991 <48780352+vutienluc1991@users.noreply.github.com>
2020-02-21 18:36:23 +0700

    Update ble.cpp

--- ble.cpp
+++ ble.cpp
@@ -1,1 +1,25 @@
 
+#include "ble.h"
+
+bool BLE::ble_init(char* name){
+  this->ble.begin("mushroom");
+  
+  // if it has nothing to check always return 1 then
+  return 1;
+}
+
+bool BLE::ble_read_data(char* buf, int size){
+  if(this->ble->available()){
+    while(this->ble->available()){
+      
+      
+    }
+  
+}
+bool BLE::ble_write_data(char* data){
+  
+  }
+
+  // if has nothing to check always return 1 then
+  return 1;
+}
